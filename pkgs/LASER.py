569dacc6cc866ef83bf8bb42e5581c8e7e6806f9
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2020-04-09 12:33:27 +0800

    Add files via upload

--- pkgs/LASER.py
+++ pkgs/LASER.py
@@ -3,7 +3,7 @@ import numpy as np
 import subprocess
 import platform #for OS name
 
-address = '127.0.0.1:8050'
+address = '192.168.99.100:8050'
 
 def check_if_laser_start():
     if isinstance(get_vect('Hello World'), (list, tuple, np.ndarray)):
@@ -11,11 +11,11 @@ def check_if_laser_start():
     else:
         raise ConnectionError('LASER port not set up. Please visit LASER\'s github to view the instructions in setting up the docker container.')
 
-def get_vect(query_in, lang='en', address=address)
+def get_vect(query_in, lang='en', address=address):
     url = 'http://' + address + '/vectorize'
     params = {'q':query_in, 'lang':lang}
     resp = requests.get(url=url, params=params).json()
     return resp['embedding']
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     check_if_laser_start()
